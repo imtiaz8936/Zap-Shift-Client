51e5fc9e3247d96a1ff161571fe26dc8314a48bd
Imtiazul Hoque <imtiazulhoque8936@gmail.com>
2025-12-03 12:53:21 +0600

    registration form updated with imgUpload field

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -8,8 +8,9 @@ const Register = () => {
     formState: { errors },
   } = useForm();
 
-  const handleRegistration = (data) => {
-    console.log("form submitted", data.email, data.password);
+  const handleRegistration = async (data) => {
+    const { name, image, email, password } = data;
+    console.log({ name, image, email, password });
   };
 
   return (
@@ -17,15 +18,56 @@ const Register = () => {
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <form onSubmit={handleSubmit(handleRegistration)} className="card-body">
           <fieldset className="fieldset">
+            <label className="label">Name</label>
+            <input
+              type="name"
+              {...register("name", {
+                required: true,
+                maxLength: {
+                  value: 20,
+                  message: "Name cannot exceed 20 characters",
+                },
+              })}
+              className="input"
+              placeholder="Your Name"
+            />
+            {errors.name && (
+              <p className="text-red-500 text-xs mt-2">{errors.name.message}</p>
+            )}
+
+            <label className="label">Profile Image</label>
+            <input
+              type="file"
+              {...register("image", { required: "Image is required" })}
+              className="file-input"
+            />
+            {errors.image && (
+              <p className="text-red-500 text-xs mt-2">
+                {errors.image.message}
+              </p>
+            )}
+
             <label className="label">Email</label>
             <input
               type="email"
-              {...register("email", { required: true })}
+              {...register("email", {
+                required: "This field is required",
+                maxLength: {
+                  value: 30,
+                  message: "Email cannot exceed 30 characters",
+                },
+                pattern: {
+                  value: /@/,
+                  message: "Email must contain an @ symbol",
+                },
+              })}
               className="input"
               placeholder="Email"
             />
-            {errors.email?.type === "required" && (
-              <p className="text-red-500">Email is required</p>
+            {errors.email && (
+              <p className="text-red-500 text-xs mt-2">
+                {errors.email.message}
+              </p>
             )}
 
             <label className="label">Password</label>
@@ -33,14 +75,23 @@ const Register = () => {
               type="password"
               {...register("password", {
                 required: true,
-                minLength: 6,
-                pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/,
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters long",
+                },
+                pattern: {
+                  value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/,
+                  message:
+                    "Password must contain one uppercase, one lowercase letter and one special character",
+                },
               })}
               className="input"
               placeholder="Password"
             />
-            {errors.password?.type === "required" && (
-              <p className="text-red-500">Password is required</p>
+            {errors.password && (
+              <p className="text-red-500 text-xs mt-2">
+                {errors.password.message}
+              </p>
             )}
             <div>
               <a className="link link-hover">Forgot password?</a>
